ea97dcfd362aee919b3cbdce3300e65ea3b97a86
Sergen Çetin <57778310+kharoooN@users.noreply.github.com>
2022-05-24 17:06:44 +0300

    Add files via upload

---
+++ ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceApp.Business.Abstract;
+using ECommerceApp.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.WebUI.ViewComponents
+{
+    public class CategoryListViewComponent : ViewComponent
+    {
+        private ICategoryService _categoryService;
+        public CategoryListViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(new CategoryListViewModel()
+            {
+                SelectedCategory = RouteData.Values["category"]?.ToString(),
+                Categories = _categoryService.GetAll()
+            });
+        }
+    }
+}
