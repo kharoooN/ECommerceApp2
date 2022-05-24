a258c71fd07f2df3aa602962c93c86c55e00194b
Sergen Çetin <57778310+kharoooN@users.noreply.github.com>
2022-05-24 16:43:35 +0300

    Add files via upload

---
+++ ECommerceApp.Entities/OrderLine.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceApp.Entities
+{
+    
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ ECommerceApp.Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceApp.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal? Price { get; set; }
+
+        public List<ProductCategory> ProductCategories { get; set; }
+    }
+}
